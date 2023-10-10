70902a407da63c87f741512752a38ca3fb384aaf
Ritchie Cervantes <ritchie.cervantes@gmail.com>
2023-10-10 15:14:37 -0500

    feat: enable favorites from dog card

---
+++ actions/favoriteDog.ts
@@ -0,0 +1,91 @@
+"use server";
+
+import getCurrentUser from "@/actions/getCurrentUser";
+import prisma from "@/lib/db";
+import { SerializableFavorite } from "@/types";
+
+interface IFavoriteDog {
+  dogIdToFavorite: string;
+}
+
+export const favoriteDog = async ({ dogIdToFavorite }: IFavoriteDog) => {
+  try {
+    const currentUser = await getCurrentUser();
+    if (!currentUser) return null;
+
+    const favorites = [...(currentUser.favorites || [])];
+
+    const existingFavorite = await prisma.favorite.findFirst({
+      where: {
+        userId: currentUser.id,
+        dogId: dogIdToFavorite,
+      },
+    });
+
+    if (existingFavorite) {
+      return { message: "dog has already been favorited" };
+    }
+
+    const newFavorite = await prisma.favorite.create({
+      data: {
+        dogId: dogIdToFavorite,
+        userId: currentUser.id,
+      },
+    });
+
+    const serializedNewFavorite: SerializableFavorite = {
+      ...newFavorite,
+      createdAt: newFavorite.createdAt.toISOString(),
+    } as SerializableFavorite;
+
+    favorites.push(serializedNewFavorite);
+
+    const user = await prisma.user.update({
+      where: {
+        id: currentUser.id,
+      },
+      data: {
+        favorites: {
+          connect: favorites.map((favorite) => ({
+            dogId_userId: {
+              dogId: favorite.dogId,
+              userId: currentUser.id,
+            },
+          })),
+        },
+      },
+    });
+
+    return { message: "dog has been favorited successfully" };
+  } catch (error: any) {
+    console.error(error);
+    throw new Error(error);
+  }
+};
+
+interface IUnfavoriteDog {
+  dogIdToUnfavorite: string;
+}
+
+export const unfavoriteDog = async ({ dogIdToUnfavorite }: IUnfavoriteDog) => {
+  try {
+    const currentUser = await getCurrentUser();
+    if (!currentUser) throw new Error("No current user");
+
+    const favorites = [...(currentUser.favorites || [])];
+    const [favoriteToRemove] = favorites.filter(
+      (favorite) => favorite.dogId === dogIdToUnfavorite,
+    );
+
+    const deletedFavorite = await prisma.favorite.delete({
+      where: {
+        id: favoriteToRemove?.id,
+      },
+    });
+
+    return { message: "dog has been unfavorited successfully" };
+  } catch (error: any) {
+    console.error(error);
+    throw new Error(error);
+  }
+};
