33aa4b53af235ab56ff0b2d1bc786d6c82f4ae86
Ritchie Cervantes <ritchie.cervantes@gmail.com>
2023-10-10 11:03:08 -0500

    Add shadcn config, toaster for project, new favicon, and redirect all requests to home page
    Forgot a dash when amending so these were changes made bewteen since last commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   test: {
     include: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
     globals: true,
