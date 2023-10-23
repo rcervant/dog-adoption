baa81cde62ea9f0cf7a44b4fbd6754281131caad
Ritchie Cervantes <ritchie.cervantes@gmail.com>
2023-10-22 21:49:13 -0500

    feat: Add valid parameter constant and mapping
    In this commit:
    - Added new valid parameter constants for search functionality.
    - Created a mapping of valid parameters for better organization and validation.
    
    This change introduces new constants for valid search parameters and provides a mapping to improve the handling of valid parameters during search operations.

--- lib/constants.ts
+++ lib/constants.ts
@@ -31,12 +31,34 @@ export const QUESTION_MARK = "?";
 // UI constants
 export const NUM_DOGS_TO_DISPLAY = 4;
 export const START_DOG_INDEX = 0;
+export const SIGN_IN = "sign-in";
+export const REGISTER = "register";
 
 // Search constants
 export const DEFAULT_SEARCH_RADIUS_KM = 25;
 
 // Geo constants
 export const EARTH_RADIUS_KM = 6371;
+
+// valid search params
+
+export const BREEDS = "breeds";
+export const ZIPCODES = "zipCodes";
+export const AGE_MIN = "ageMin";
+export const AGE_MAX = "ageMax";
+export const SIZE = "size";
+export const FROM = "from";
+export const SORT = "sort";
+
+export const VALID_PARAMS = {
+  breeds: BREEDS,
+  zipCodes: ZIPCODES,
+  ageMin: AGE_MIN,
+  ageMax: AGE_MAX,
+  size: SIZE,
+  from: FROM,
+  sort: SORT,
+};
 
 // Other constants
 export const CURR_SORT_INDEX = 0;
