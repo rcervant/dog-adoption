c78cd4fc145a4b83c461fab4b68f1a9f398ffe53
Ritchie Cervantes <ritchie.cervantes@gmail.com>
2023-10-24 13:59:46 -0500

    refactor: Set 'Register' as default tab in AuthTab

--- app/(auth)/_components/AuthTab.tsx
+++ app/(auth)/_components/AuthTab.tsx
@@ -9,16 +9,16 @@ import SignInForm from "./SignInForm";
 
 const AuthTab = () => {
   return (
-    <Tabs defaultValue={SIGN_IN} className="w-[400px]">
+    <Tabs defaultValue={REGISTER} className="w-[400px]">
       <TabsList className="grid w-full grid-cols-2">
-        <TabsTrigger value={SIGN_IN}>Sign In</TabsTrigger>
         <TabsTrigger value={REGISTER}>Register</TabsTrigger>
+        <TabsTrigger value={SIGN_IN}>Sign In</TabsTrigger>
       </TabsList>
-      <TabsContent value={SIGN_IN}>
-        <SignInForm value={SIGN_IN} />
-      </TabsContent>
       <TabsContent value={REGISTER}>
         <SignInForm value={REGISTER} />
+      </TabsContent>
+      <TabsContent value={SIGN_IN}>
+        <SignInForm value={SIGN_IN} />
       </TabsContent>
     </Tabs>
   );
